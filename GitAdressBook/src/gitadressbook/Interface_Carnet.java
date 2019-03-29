d38ad0263151533c9f3dfd867c137e6030764fc1
clavaud <clavaud@orange.fr>
2019-03-29 09:10:23 +0100

    ajout liste

--- GitAdressBook/src/gitadressbook/Interface_Carnet.java
+++ GitAdressBook/src/gitadressbook/Interface_Carnet.java
@@ -1,6 +1,7 @@
 package gitadressbook;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,6 +37,9 @@ public class Interface_Carnet extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -48,6 +52,28 @@ public class Interface_Carnet extends javax.swing.JFrame {
         txtemail = new javax.swing.JTextField();
         btnenr = new javax.swing.JButton();
         btnan = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        txtid = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        rechnom = new javax.swing.JTextField();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+
+        jButton1.setText("jButton1");
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -77,10 +103,52 @@ public class Interface_Carnet extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Recherche par ID");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        txtid.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtidActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Supprimer");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Recherche par nom");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setText("Supprimer");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        jButton6.setText("Liste");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(49, 49, 49)
+                .addComponent(btnenr)
+                .addGap(101, 101, 101)
+                .addComponent(btnan)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,13 +163,25 @@ public class Interface_Carnet extends javax.swing.JFrame {
                     .addComponent(txtcontact)
                     .addComponent(txtprenom)
                     .addComponent(txtnom))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(68, Short.MAX_VALUE)
-                .addComponent(btnenr)
-                .addGap(66, 66, 66)
-                .addComponent(btnan)
-                .addGap(39, 39, 39))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(txtid, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(rechnom, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton6)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -111,24 +191,31 @@ public class Interface_Carnet extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(txtnom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtnom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtid, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(txtprenom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtprenom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton4)
+                    .addComponent(rechnom, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton5))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(txtcontact, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtcontact, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton6))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(txtemail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnenr)
                     .addComponent(btnan))
-                .addContainerGap())
+                .addGap(18, 18, 18))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -152,14 +239,14 @@ public class Interface_Carnet extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnenrActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnenrActionPerformed
-        
+
         String req;
         // prepare notre sql de création
-        
+
         req = "insert into carnet (`nom`, `prenom`, `contact`, `mail`) values (?,?,?,?)";
         try {
             PreparedStatement ps = maConnexion.getConnexion().prepareStatement(req);
-            
+
             // recuperation des champs de l'écran
             String nom = txtnom.getText();
             String prenom = txtprenom.getText();
@@ -170,14 +257,16 @@ public class Interface_Carnet extends javax.swing.JFrame {
             ps.setString(2, prenom);
             ps.setString(3, contact);
             ps.setString(4, email);
-            
+
             // execution de ma requetes préparées
             ps.executeUpdate();
-            
+
             // popup réussite
             JOptionPane.showMessageDialog(this, "Requête executée avec succés.");
-            
+
             // RAZ de mes champs
+            txtid.setText("");
+            rechnom.setText("");
             txtnom.setText("");
             txtprenom.setText("");
             txtcontact.setText("");
@@ -188,14 +277,121 @@ public class Interface_Carnet extends javax.swing.JFrame {
         }
 
     }//GEN-LAST:event_btnenrActionPerformed
-
+    // boutton annuler
     private void btnanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnanActionPerformed
         // RAZ de mes champs
+        txtid.setText("");
+        rechnom.setText("");
         txtnom.setText("");
         txtprenom.setText("");
         txtcontact.setText("");
         txtemail.setText("");
     }//GEN-LAST:event_btnanActionPerformed
+
+    // recherche par ID
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+        String req;
+        // préparation de la requete de sélection
+        req = "select * from carnet where id = '" + txtid.getText() + "'";
+        try {
+            PreparedStatement ps = maConnexion.getConnexion().prepareStatement(req);
+            //
+            ResultSet resultat;
+            resultat = ps.executeQuery(req);
+            // tant qu'il a des données correspondant à notre sélect-> on recupere les donnée
+            while (resultat.next()) {
+                txtnom.setText(resultat.getString((2)));     // position 2 -> nom
+                txtprenom.setText(resultat.getString((3)));     // position 3 -> prenom
+                txtcontact.setText(resultat.getString((4)));     // position 4 -> contact
+                txtemail.setText(resultat.getString((5)));     // position 5 -> mail
+
+            }
+            // TODO exception si enregistrement non trouve 
+        } catch (SQLException ex) {
+            Logger.getLogger(Interface_Carnet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void txtidActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtidActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtidActionPerformed
+    // supprimer données dans carnet
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        String req;
+        req = "delete from carnet where id =?";
+        try {
+            PreparedStatement ps = maConnexion.getConnexion().prepareStatement(req);
+            ps.setInt(1, Integer.parseInt(txtid.getText()));
+            int count = ps.executeUpdate();
+            JOptionPane.showMessageDialog(this, count + " Donnée supprimer avec succés");
+
+            // mise à blanc des zones de l'écran
+            txtid.setText("");
+            rechnom.setText("");
+            txtnom.setText("");
+            txtprenom.setText("");
+            txtcontact.setText("");
+            txtemail.setText("");
+        } catch (SQLException ex) {
+            Logger.getLogger(Interface_Carnet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+    }//GEN-LAST:event_jButton3ActionPerformed
+    // recherche par nom
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        String req;
+        // préparation de la requete de sélection
+        req = "select * from carnet where nom = '" + rechnom.getText() + "'";
+        try {
+            PreparedStatement ps = maConnexion.getConnexion().prepareStatement(req);
+            //
+            ResultSet resultat;
+            resultat = ps.executeQuery(req);
+            // tant qu'il a des données correspondant à notre sélect-> on recupere les donnée
+            while (resultat.next()) {
+                txtnom.setText(resultat.getString((2)));     // position 2 -> nom
+                txtprenom.setText(resultat.getString((3)));     // position 3 -> prenom
+                txtcontact.setText(resultat.getString((4)));     // position 4 -> contact
+                txtemail.setText(resultat.getString((5)));     // position 5 -> mail
+
+            }
+        } catch (SQLException ex) {
+            // TODO exception si enregistrement non trouve 
+            Logger.getLogger(Interface_Carnet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
+    // supprimer /nom dans carnet
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        String req;
+        req = "delete from carnet where nom =?";
+        try {
+            PreparedStatement ps = maConnexion.getConnexion().prepareStatement(req);
+            // TODO ps.setText(1, String(rechnom.getText()));
+            int count = ps.executeUpdate();
+            JOptionPane.showMessageDialog(this, count + " Donnée supprimer avec succés");
+
+            // mise à blanc des zones de l'écran
+            txtid.setText("");
+            rechnom.setText("");
+            txtnom.setText("");
+            txtprenom.setText("");
+            txtcontact.setText("");
+            txtemail.setText("");
+        } catch (SQLException ex) {
+            Logger.getLogger(Interface_Carnet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton5ActionPerformed
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {
+
+        // on instancie notre objet tableau
+        Tableau tableau = new Tableau();
+
+        // on le rend visible
+        tableau.setVisible(true);
+    }
 
     /**
      * @param args the command line arguments
@@ -234,14 +430,24 @@ public class Interface_Carnet extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnan;
     private javax.swing.JButton btnenr;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField rechnom;
     private javax.swing.JTextField txtcontact;
     private javax.swing.JTextField txtemail;
+    private javax.swing.JTextField txtid;
     private javax.swing.JTextField txtnom;
     private javax.swing.JTextField txtprenom;
     // End of variables declaration//GEN-END:variables
